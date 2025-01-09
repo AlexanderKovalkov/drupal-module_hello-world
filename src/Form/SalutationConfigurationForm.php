45d9d92c4980114c13723281b65eff27ede9eb39
Alexander Kovalkov <a.v.kovalkov@gmail.com>
2025-01-09 12:27:16 +0300

    added first step of logger

--- src/Form/SalutationConfigurationForm.php
+++ src/Form/SalutationConfigurationForm.php
@@ -4,12 +4,46 @@ namespace Drupal\hello_world\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Configuration form definition for the salutation message.
  */
 class SalutationConfigurationForm extends ConfigFormBase
 {
+
+    /**
+     * @var \Drupal\Core\Logger\LoggerChannelInterface
+     */
+    protected $logger;
+
+    /**
+     * SalutationConfigurationForm constructor.
+     *
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+     *   Фабрика конфигурации.
+     * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+     *   Канал логирования.
+     */
+    public function __construct(ConfigFactoryInterface $config_factory, LoggerChannelInterface $logger)
+    {
+        parent::__construct($config_factory);
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('config.factory'),
+            $container->get('hello_world.logger.channel.hello_world')
+        );
+    }
 
     /**
      * {@inheritdoc}
@@ -55,5 +89,9 @@ class SalutationConfigurationForm extends ConfigFormBase
             ->save();
 
         parent::submitForm($form, $form_state);
+
+        $this->logger->info('The Hello World salutation has been changed to @message.', [
+            '@message' => $form_state->getValue('salutation'),
+        ]);
     }
 }
